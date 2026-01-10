4b444ef2ea6528d95f9f542f77a17654f39d45a5
Ariel Michalik <arie.michalik@gmail.com>
2026-01-11 00:36:42 +0100

    provided creating order on checkout

--- src/api/order-service.ts
+++ src/api/order-service.ts
@@ -1,4 +1,4 @@
-import type { OrderDetailsResponse } from "../types/orders";
+import type { OrderCreateRequest, OrderCreateResponse, OrderDetailsResponse } from "../types/orders";
 import { API_BASE_URL, apiCall } from "./utils";
 
 export const fetchUserOrders = async (): Promise<OrderDetailsResponse[]> => {
@@ -51,6 +51,33 @@ export const fetchAllOrders = async (): Promise<OrderDetailsResponse[]> => {
     return await response.json();
   } catch (error) {
     console.error("API Error fetching user orders:", error);
+    throw error;
+  }
+};
+
+export const createOrder = async (order: OrderCreateRequest): Promise<OrderCreateResponse> => {
+  const jwtToken = localStorage.getItem("token");
+  if (!jwtToken) {
+    throw new Error("Authorization token not found. Please log in.");
+  }
+
+  try {
+    const response = await apiCall(`${API_BASE_URL}/api/orders/grpc`, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${jwtToken}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(order),
+    });
+
+    if (!response.ok) {
+      throw new Error(`Failed to create order: ${response.status} ${response.statusText}`);
+    }
+
+    return await response.json();
+  } catch (error) {
+    console.error("API Error creating order:", error);
     throw error;
   }
 };
